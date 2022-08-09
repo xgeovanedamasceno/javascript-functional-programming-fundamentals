a260351668279842d2a1115ecf48d3dbacdbc561
xgeovanedamasceno <xgeovanedamasceno@gmail.com>
2022-08-09 18:39:49 -0300

    add an example of arrow function

---
+++ basics/function-anatomy2.js
@@ -0,0 +1,9 @@
+// Arrow function
+
+const greet = (name) => `Hello, ${name}!` // -> string interpolation
+
+console.log(greet("John"))
+console.log(greet("Philip"))
+const greetMessage = greet("Ann")
+
+console.log(greetMessage)
